b5dc0cddc5d190dd8496cc5daef0a5727690a100
Adam Patterson <fakeempire@gmail.com>
2011-04-11 17:23:14 -0500

    fix indentation in views.py

--- tumblelog/views.py
+++ tumblelog/views.py
@@ -4,8 +4,7 @@ from tumblelog.models import TumbleItem
 
 
 def tumbler(request):
-	context = {
-		'tumble_item_list': TumbleItem.objects.all().order_by('-pub_date')
-	}
-
-	return render_to_response('tumblelog/list.html', context)
+    context = {
+        'tumble_item_list': TumbleItem.objects.all().order_by('-pub_date')
+        }
+    return render_to_response('tumblelog/list.html', context)
