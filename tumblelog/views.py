b6d2db2449fb6c618e45aa86457394257e1ee2cb
Clayton Miller <pimpc@claytonmiller.com>
2011-04-11 13:49:03 -0500

    initial commit

---
+++ tumblelog/views.py
@@ -0,0 +1,18 @@
+from tumblelog.models import TumbleItem
+
+from django.shortcuts import render_to_response
+
+
+
+def tumbler(request):
+
+	context = {
+
+		'tumble_item_list': TumbleItem.objects.all().order_by('-pub_date')
+
+	}
+
+
+
+	return render_to_response('tumblelog/list.html', context)
+
