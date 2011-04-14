6ff5931ca2c9595304e14e93a457351bcccf37b3
Adam Patterson <fakeempire@gmail.com>
2011-04-14 18:26:36 -0500

    fix blog detail

--- blog/urls.py
+++ blog/urls.py
@@ -1,39 +1,11 @@
 from django.conf.urls.defaults import *
+from django.views.generic.list_detail import object_detail
 
 from testproject.blog.models import Entry
-
 from tagging.views import tagged_object_list
 
+info_dict = {'queryset': Entry.objects.all() }
 
-
-info_dict = {
-
-	'queryset': Entry.objects.filter(status=1),
-
-	'date_field': 'pub_date',
-
-}
-
-
-
-urlpatterns = patterns('django.views.generic.date_based',
-
-	(r'(?P<year>d{4})/(?P<month>[a-z]{3})/(?P<day>w{1,2})/(?P<slug>[-w]+)/$', 'object_detail', dict(info_dict, slug_field='slug',template_name='blog/detail.html')),
-
-	(r'^(?P<year>d{4})/(?P<month>[a-z]{3})/(?P<day>w{1,2})/(?P<slug>[-w]+)/$', 'object_detail', dict(info_dict, template_name='blog/list.html')),
-
-	(r'^(?P<year>d{4})/(?P<month>[a-z]{3})/(?P<day>w{1,2})/$','archive_day',dict(info_dict,template_name='blog/list.html')),
-
-	(r'^(?P<year>d{4})/(?P<month>[a-z]{3})/$','archive_month', dict(info_dict, template_name='blog/list.html')),
-
-	(r'^(?P<year>d{4})/$','archive_year', dict(info_dict, template_name='blog/list.html')),
-
-	(r'^$','archive_index', dict(info_dict, template_name='blog/list.html')),
-
-)
-
-
-
-
-
- 
+urlpatterns = patterns('',
+                       url(r'^(?P<object_id>\d]+)/$', object_detail, info_dict),
+                       )
